fe6f4e5b9ffef8af347db4862e89601ce2a11592
sushant0 <sushant@freshdesk.com>
2016-05-11 19:23:33 +0530

    testing

--- hello.js
+++ hello.js
@@ -1,1 +1,10 @@
+var http = require("http");
+
+var server = http.createServer(function(request,response){
+	response.writeHead(200,{"content-type":"text/html"});
+	response.write("hello world");
+	response.end();
+});
+
+server.listen(8000);
 console.log('hello world 123');
