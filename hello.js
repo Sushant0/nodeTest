c4949aa05fb731da5c3552fcfc9705135e9afc70
sushant0 <sushant@freshdesk.com>
2016-05-14 15:49:29 +0530

    DB

--- hello.js
+++ hello.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var pg = require("pg");
 
 var server = http.createServer(function(request,response){
 	response.writeHead(200,{"content-type":"text/html"});
@@ -7,10 +8,18 @@ var server = http.createServer(function(request,response){
 });
 
 // http.set('port', (process.env.PORT || 5000));
+// postgres://fkfzvpdebvbzdh:qICgOMOWPnx9IPifa2@*HOST*:*PORT:/*DATABASE*
 var PORT = (process.env.PORT || 5000);
-var connectionString = process.env.DATABASE_URL;
+var connectionString = "postgres://fkfzvpdebvbzdh:qICgOMOWPnx9IPifa2-FqZDoBE@ec2-54-243-249-165.compute-1.amazonaws.com:5432/dei9lujiig8mcj";
 
 server.listen(PORT,function(){
 	console.log("Server listening on: http://localhost:%s %s", PORT , connectionString);
 });
-console.log('hello world' + process.env.PORT + connectionString);
+console.log('hello world' + process.env.PORT + connectionString + pg);
+
+var client = new pg.Client(connectionString);
+client.connect();
+var query1 = client.query('CREATE TABLE IF NOT EXISTS menu_table(day int not null, time int not null, menu text, PRIMARY KEY(day, which_food))');
+query1.on('end', function() {
+   console.log("Menu today table successfully created");
+});
