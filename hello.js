885feaa8d28ac039fe5f8c8b6e9f5579f4c29c73
sushant0 <sushant@freshdesk.com>
2016-05-14 15:35:01 +0530

    DataBase url

--- hello.js
+++ hello.js
@@ -8,7 +8,9 @@ var server = http.createServer(function(request,response){
 
 // http.set('port', (process.env.PORT || 5000));
 var PORT = (process.env.PORT || 5000);
+var connectionString = process.env.DATABASE_URL;
+
 server.listen(PORT,function(){
-	console.log("Server listening on: http://localhost:%s", PORT);
+	console.log("Server listening on: http://localhost:%s %s", PORT , connectionString);
 });
-console.log('hello world' + process.env.PORT);
+console.log('hello world' + process.env.PORT + connectionString);
